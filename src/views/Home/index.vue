fc602f59f74b990f45b53f77a6ffc344b58c118f
lm <984425247@qq.com>
2019-12-19 11:14:02 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>主页</div>
+  <div>
+      主页
+
+  </div>
 </template>
 
 <script>
@@ -9,5 +12,10 @@ export default {
 </script>
 
 <style>
+/* 默认情况下，vue单文件组件的style样式时全局的，
+我们现在指向改变当前组件的样式不会影响其它用到的属性scoped
+scoped属性可以控制当前组件 */
+/* 这里不支持less  lang属性可以改变其默认改成less */
+/* 在单文件组件中,如果需要在style标签中使用 诸如 **less** **scss**  需要在style标签上 给lang属性赋值 */
 
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,114 @@
 <template>
-  <div>登录</div>
+  <div class='login'>
+      <!-- 使用elementul -->
+<el-card class='login-card'>
+    <!-- 引入title -->
+    <div  class='title'>
+        <img src="../../assets/img/logo_index.png" alt="">
+    </div>
+         <!-- 表单 => el-form包裹 -->
+         <!-- 手机号 -->
+    <el-form  ref="myform" :rules="loginRules" :model="loginForm" >
+        <!-- prop校验属性，值时要校验的字段名 -->
+        <el-form-item  prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <!-- 验证码输入框 -->
+
+        <el-form-item  prop="code" >
+            <el-input   v-model="loginForm.code" style="width:50%" placeholder="请输入验证码"></el-input>
+            <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+
+    <!-- 复选框 -->
+     <el-form-item  prop="check">
+                 <!-- 同意选项 -->
+        <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+     </el-form-item>
+     <!-- 提交 -->
+     <el-form-item>
+                  <!-- 登录按钮 -->
+        <el-button @click="submitLogin" type="primary" style="width:100%">登录</el-button>
+    </el-form-item>
+    </el-form>
+
+</el-card>
+</div>
 </template>
 
 <script>
+// import { log } from 'util'
 export default {
+  data () {
+    return {
+      // loginForm代表整个form表单数据对象
+      loginForm: {
+        // mobile代表手机输入框
+        mobile: '',
+        // 代表验证的输入框
+        code: '',
+        // 复选框
+        check: false
 
+      },
+      // 验证表单对象
+      loginRules: {
+        // 验证规则对象key value []
+        mobile: [{ required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+        code: [{ required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码为6位数字' }],
+        check: [{ validator: function (rule, value, callback) {
+        // validator自定义校验函数
+        // 第一个参数时rule校验规则没啥用
+        // 第二个参数是value要校验的字段值
+        // 第三个值是callback回调函数
+          if (value) {
+          // 如果当前是true代表已经勾选
+            callback()
+          } else {
+          // 如果当前为false代表没有勾选
+            callback(new Error('请点击勾选'))// 如果没有勾选代表校验规则失败,应该停止
+          }
+        } }]
+
+      }
+
+    }
+  },
+  methods: {
+    // 手动校验
+    submitLogin () {
+      this.$refs.myform.validate(function (isok) {
+        if (isok) {
+          console.log('校验成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.login{
+    background: url(../../assets/img/loginIMG.jpg);
+    height:100vh;
+    background-size: cover;
+    display:flex;
+    justify-content:center;
+    align-items: center;
+    .login-card{
+        width: 400px;
+        height: 350px;
+        .title{
+            text-align: center;
+            img{
+                height: 25px;
+
+            }
+        }
+    }
+
+}
 
 </style>
