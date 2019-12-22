78bffbf2febb285184a64b66e4d923fa7412c41b
lm <984425247@qq.com>
2019-12-22 10:53:49 +0800

    黑马头条PC-面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- 面包屑组件 -->
+  <el-breadcrunb separator=">">
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>
+          <slot name="title"></slot>
+      </el-breadcrumb-item>
+
+  </el-breadcrunb>
+  <!-- 去components/index.js下面注册 -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -4,14 +4,16 @@
 // import layOutHeader from './home/layout-header'
 import layOutAside from './home/layout-aside'
 import layOutAheader from './home/layout-header'
+import breakCrumb from './common/bread-crumb'
 
 export default {
 
   install (Vue) {
     // console.log(Vue)
-
+    // 全局注册，再任意位置都可以使用
     Vue.component('layout-aside', layOutAside)
     Vue.component('layout-header', layOutAheader)
+    Vue.component('bread-crumb', breakCrumb)// 面包屑组件
     //  注册组件
     //     Vue.component('layout-aside', layOutAside )// 注册全局组件因为再main.js中的vue实力前引入了所以属于全局注册，以后再加组件可以引入在这注册，任何位置都能使用
     //     Vue.component('layout-header',  layOutHeader )
