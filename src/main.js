4b8e6433045e1cc908ea59740248bd4b06cbc14b
lm <984425247@qq.com>
2019-12-22 11:06:05 +0800

    黑马头条PC-评论列表-新建页面-挂载路由

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import axios from './utils/request'
 import Component from './components'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less'// 引入初始化样式
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/app/v1_0'
+
 Vue.prototype.$axios = axios// axios赋值给全局属性
 // 注册
 Vue.use(ElementUI)
