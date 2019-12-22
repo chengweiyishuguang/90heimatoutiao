9af59e7a53db8adb06481b4f8036a4c15bbeae0f
lm <984425247@qq.com>
2019-12-22 11:26:42 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/components/common/bread-crumb.vue
+++ src/components/common/bread-crumb.vue
@@ -1,12 +1,12 @@
 <template>
 <!-- 面包屑组件 -->
-  <el-breadcrunb separator=">">
+  <el-breadcrumb separator=">">
       <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
       <el-breadcrumb-item>
           <slot name="title"></slot>
       </el-breadcrumb-item>
 
-  </el-breadcrunb>
+  </el-breadcrumb>
   <!-- 去components/index.js下面注册 -->
 </template>
 
