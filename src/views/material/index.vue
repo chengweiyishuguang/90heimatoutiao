5405f0cdddcfafa478438019255366958bfe4ab5
lm <984425247@qq.com>
2019-12-23 16:30:13 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -33,6 +33,19 @@
 
           </el-tab-pane>
       </el-tabs>
+      <!-- 公共分页组件 -->
+ <el-row type="flex" justify="center">
+            <el-pagination
+              background
+              layout="prev, pager, next"
+              :total="page.total"
+              :page-size="page.pageSize"
+              :current-page="page.currentPage"
+              @current-change="changePage"
+
+              >
+            </el-pagination>
+         </el-row>
   </el-card>
 </template>
 
@@ -42,12 +55,24 @@ export default {
     return {
       activeName: 'all', // 当前选中的标签
       // 定义一个list 接收素材数据数据
-      list: []
+      list: [],
+      page: {
+        currentPage: 1, // 默认页码
+        pageSize: 8, // 默认每页条数
+        total: 0// 总条数
+
+      }
     }
   },
   methods: {
+    // 改变页码的方法
+    changePage (newpage) {
+      this.page.currentPage = newpage
+      this.getMaterial()
+    },
     // 切换页签的方法
     changetab () {
+      this.page.currentPage = 1// 重置回第一页
       this.getMaterial()
     },
     // 获取素材的方法
@@ -55,10 +80,14 @@ export default {
       this.$axios({
         url: '/user/images',
         params: {
+          page: this.page.currentPage,
+          per_page: this.page.pageSize,
+
           collect: this.activeName === 'collect'// 传false是获取所有数据，传true是获取收藏数据
         }
       }).then(result => {
         this.list = result.data.results// 获取所有的图片数据
+        this.page.total = result.data.total_count// 总条数
       })
     }
 
