3e706e8402916ae2e10c4ff062ff9a54884ecb15
lm <984425247@qq.com>
2019-12-23 13:36:09 +0800

    黑马头条PC-素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,15 +3,23 @@
       <bread-crumb slot="header">
       <template slot="title">
           素材管理
-      </template>
 
+      </template>
       </bread-crumb>
+      <el-tabs v-model="activeName">
+          <el-tab-pane label="全部图片" name='all'></el-tab-pane>
+          <el-tab-pane label="收藏图片" name="collect"></el-tab-pane>
+      </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'// 当前选中的标签
+    }
+  }
 }
 </script>
 
