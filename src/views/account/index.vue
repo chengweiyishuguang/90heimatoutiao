d89c54f18028d587c9965525d1ccad814c91e3c1
lm <984425247@qq.com>
2019-12-28 21:42:03 +0800

    黑马头条PC-账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,6 +5,23 @@
             账户信息
         </template>
       </bread-crumb>
+      <el-form  style="margin-left:100px" label-width="100px">
+          <el-form-item label="用户名">
+              <el-input style="width:40%"></el-input>
+          </el-form-item>
+          <el-form-item label="简介">
+              <el-input style="width:40%"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+              <el-input style="width:40%"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号">
+              <el-input disabled style="width:40%"></el-input>
+          </el-form-item>
+      </el-form>
+      <el-upload class="head-upload" action="" :show-file-list="false">
+          <img src="../../assets/img/loginIMG.jpg" alt="">
+      </el-upload>
   </el-card>
 </template>
 
@@ -14,6 +31,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+    .head-upload {
+        width: 200px;
+        height: 200px;
+        position: absolute;
+        top:200px;
+        right: 300px;
+        img {
+            width: 100%;
+            height:100%;
+            border-radius: 50%;
+        }
+    }
 
 </style>
